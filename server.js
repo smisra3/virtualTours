796fcf9257a02e80a724317a1519d52f497b8895
Shubhankar Misra <shubhankar.misra@99acres.com>
2022-05-23 14:09:21 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express()
+const port = 5500;
+
+const options = {
+  root: path.join(__dirname, ''),
+  dotfiles: 'deny',
+  headers: {
+    'x-timestamp': Date.now(),
+    'x-sent': true
+  }
+};
+
+app.get('/', (req, res) => {
+  res.sendFile('3dview.html', options);
+});
+
+app.get('/OrbitControls.js', (req, res) => {
+  res.sendFile('OrbitControls.js', options);
+});
+
+app.get('/:fileName', (req, res) => {
+  res.sendFile(`/assets/${req.params.fileName}`, options);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
