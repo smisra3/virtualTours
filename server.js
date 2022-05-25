34b93938ec1586c8791c29953d26d3f14ceb6a37
Shubhankar Misra <shubhankar.misra@99acres.com>
2022-05-25 13:43:45 +0530

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express()
-const port = 5500;
+const port = 5500; 
 
 const options = {
   root: path.join(__dirname, ''),
